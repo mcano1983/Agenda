2c56593c99d667720ed7ee64bab8ba0ea9f4703a
MiguelAngel <mcano.1983@gmail.com>
2018-11-26 12:42:33 +0100

    Creacion variables clase contacto

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,17 @@
+package org.iesalandalus.programacion.agenda;
+	//Inicializacion de variables privativas//
+public class Contacto {
+	private String nombre;
+	private String telefono;
+	private String correo;
+	//Creacion de metodos get para variables de la clase Contacto//
+	public String getnombre() {
+		return this.nombre;
+		}
+	public String gettelefono() {
+		return this.telefono;
+		}
+	public String getcorreo() {
+		return this.correo;
+	}
+}
