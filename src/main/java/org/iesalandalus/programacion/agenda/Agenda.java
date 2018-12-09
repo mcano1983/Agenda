fb5272f3c84cfc342cd53b372a327a0d1df19972
MiguelAngel <mcano.1983@gmail.com>
2018-12-09 22:40:51 +0100

    Llegada hasta el metodo añadir

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -1,8 +1,12 @@
 package org.iesalandalus.programacion.agenda;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Agenda {
 	private int numContactos;
 	private static final int MAX_CONTACTOS = 1000;
+	
 
 	public int getNumContactos() {
 		return numContactos;
@@ -12,13 +16,47 @@ public class Agenda {
 	}
 
 	public Contacto[] getContactos() {
+		//crear array de contactos y copia de contactos con la longitud de numero de contactos de la agenda.
+		//copiar con arraycopy uno a otro, devolver la copia
+		
+		Contacto[] contactos = new Contacto[getNumContactos()];
+		Contacto[] copiaDeContactos = new Contacto[getNumContactos()];
+		System.out.println("dentro de getContactos"+Arrays.toString(contactos));
+		System.arraycopy(contactos, 0, copiaDeContactos, 0, contactos.length);
 
-		Contacto contactosCopia[] = new Contacto[getNumContactos()];
-		Contacto contactos[] = new Contacto[getNumContactos()];
-		Contacto a[] = new Contacto[contactosCopia.length + contactos.length];
-		System.arraycopy(contactosCopia, 0, a, 0, contactosCopia.length);
-		System.arraycopy(contactos, 0, a, contactosCopia.length, contactos.length);
-		return contactos;
+		return copiaDeContactos;
+
 	}
+	
+	
+		private boolean IndiceNoSuperaTamano(int i){
+			if(i>=getContactos().length) {
+				return true;
+			}else
+				return false;
+			}
+		//Intente hacer el metodo anadir pero no pude hacer que funcionase
+	/*public void anadir(Contacto contacto) {
+		System.out.println("el numero de contatos vale"+ this.getNumContactos());
+		Contacto[] contactosActuales = new Contacto[this.getNumContactos()];
+	 
+		int numeroContactos = this.numContactos;
+		contactosActuales = this.getContactos();
+		System.out.println("los contactos actuales son"+Arrays.toString(contactosActuales));
 
+		contacto=new Contacto(contacto.getNombre(),contacto.getTelefono(),contacto.getCorreo());
+	
+		Contacto[] contactos = new Contacto[this.getNumContactos()+1];
+			
+		contactos[contactos.length-1] = contacto;
+		
+		if(this.numContactos >= 1) {
+			for(int i= 0; i<=this.getNumContactos();i++) {
+				contactos[i] = contactosActuales[i];
+			}
+		}
+		
+		this.numContactos = contactos.length;
+		System.out.println(Arrays.toString(contactos));
+	}*/
 }
--- src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
@@ -1,12 +1,15 @@
 package org.iesalandalus.programacion.agenda;
 
+import java.util.Arrays;
+
 import javax.naming.OperationNotSupportedException;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
 
-	public static void main(String[] args) {
-		System.out.println("Programa para gestionar una agenda de contactos");
+
+
+
 	}
 	
-}
+
